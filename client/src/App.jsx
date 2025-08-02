bbbbf4e07fb9d58cce6c192b2fa2dd2c3b4f562d
SgAtjiit <161503754+SgAtjiit@users.noreply.github.com>
2025-08-02 23:06:06 +0530

    Add Form component for mood input with submit button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import {  Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Form from './pages/Form'
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-      
-        
-      </div>
-    
-    </>
-  )
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/form' element={<Form/>}/>
+
+      </Routes>
+    </>  )
 }
 
 export default App
---
+++ client/src/pages/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <input type="text" placeholder='Enter your mood here ' />
+        <button>Submit</button>
+    </div>
+  )
+}
+
+export default Form
